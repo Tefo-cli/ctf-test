4f954952cb11dd4316b48adaf83f76d19789ccf0
Mourtadi Taoufik <118552514+Tefo-cli@users.noreply.github.com>
2026-09-16 15:38:26 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
-<img src="ff" onerror="alert(1)";>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Test Local</title>
+    <!-- Appel du fichier JavaScript externe -->
+    <script>alert(1)</script>
+</head>
+<body>
+    <h1>Test d'exécution de script</h1>
+    <p>Si la boîte d'alerte s'est affichée, le script fonctionne.</p>
+</body>
+</html>
